00e6a92f517c8a77f6e9b0b41912362836b04893
jaumeabb <jaumeabb@hotmail.com>
2022-11-17 11:18:18 +0100

    Added life saver

--- Main.R
+++ Main.R
@@ -2,3 +2,6 @@
 library(tidyverse)
 
 # use origin to cerate a new branch and not upstream. Upstream permissions will be unavailable
+
+#Here's a life saver
+#https://stackoverflow.com/questions/927358/how-do-i-undo-the-most-recent-local-commits-in-git
