1b29a267491455765a7c6272b81ce69bc2d92e91
jaumeabb <jaumeabb@hotmail.com>
2022-11-17 10:38:05 +0100

    got a very nice repo

---
+++ Getting_from_shota.R
@@ -0,0 +1,3 @@
+library(usethis)
+library(here)
+create_from_github("ColeShota/FinseOS", fork = T, destdir = "C:/Users/Usuari/OneDrive - Universitat de Barcelona/Cursos/Data Reproducibility Norway 22/Course/Exercises/Ex8-1611-Night-GitHubSetting/FinseOS") #Let's fork
